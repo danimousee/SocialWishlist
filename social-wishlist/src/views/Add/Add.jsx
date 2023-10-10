54474cce5788e83be4a114b40ef6e55505ebc7e3
Veronica Gomez <veronicalauragomez@gmail.com>
2023-10-10 17:47:14 -0300

    Se agrega pantalla de agregado manual de producto
    Incluye guardado en la bbdd

--- social-wishlist/src/views/Add/Add.jsx
+++ social-wishlist/src/views/Add/Add.jsx
@@ -1,41 +1,129 @@
-import { Field, Form, Formik } from 'formik';
+import React from 'react';
+import { Formik, Form } from "formik";
+import * as yup from 'yup';
 import { useDispatch } from "react-redux";
 import { addProduct } from '../../firebase/queries/products';
-import { Container, H2 } from '../../components/Styles.css';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/SaveAlt';
+import Autocomplete from '@mui/material/Autocomplete';
+import InputAdornment from '@mui/material/InputAdornment';
+
+const URL = /^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+
+const validationSchema = yup.object({
+    name: yup.string('Ingresa el nombre').required('El nombre es requerido'),
+    images: yup.string().matches(URL, 'Ingresa una URL valida').required('La imagen es requerida'),
+    url: yup.string().matches(URL, 'Ingresa una URL valida').required('El link es requerido'),
+  //  provider: yup.required('Ingresa el provider')
+});
+
+const initialValues = {
+  name: '',
+  images: '',
+  url: '',
+  provider: { name: 'Mercado Libre', id: 'MELI' }
+};
+
+const optionsProvider = [
+  { name: 'Mercado Libre', id: 'MELI' },
+  { name: 'Facebook', id: 'FACE' },
+  { name: 'Instagram', id: 'INSTA' },
+  { name: 'Otros', id: 'OTHER' },
+];
+
 
 const Add = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = (values) => {
-   dispatch(addProduct(values))
-  }
+  const submit = values => {
 
-  return(
-    <Formik
-    initialValues={{
-        title: '',
-        description: '',
-        images: '',
-      }}
-      onSubmit={handleSubmit}
-      >
-      <Form>
-        <H2 >Cargue su producto</H2>
-      <Container>
-        <Field className="text-field" id="title" name="title" placeholder="Titulo"  />
+    //Creo array de imagenes
+    const arrayimages = []
+    arrayimages[0]= values.images
+    values.images = arrayimages
 
-        <Field className="text-field" as="textarea" id="description" name="description" placeholder="Descripcion"  />
+    values.provider = values.provider.id
+    //alert(JSON.stringify(values, null, 2));
+    dispatch(addProduct(values))
+  };
+  
+  return (
+    <Formik initialValues={initialValues} onSubmit={submit} validationSchema={validationSchema}>
+      {({ handleChange, values, setFieldValue, handleBlur, handleReset, isSubmitting, errors, touched }) => (
+        <Form>
+          <div className="main-box">
+            <div className="content-box">
+              <TextField color="primary" fullWidth
+                label="Nombre"
+                id="name"
+                name="name"
+                className='search-bar-text-field'
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.name && Boolean(errors.name)}
+                helperText={touched.name && errors.name}
+              />
 
-    {/*     <Field
-          id="images"
-          name="images"
-          placeholder="jane@acme.com"
-          type="images"
-        /> */}
-  </Container>
-        <button type="submit">Submit</button>
-      </Form>
+              <Autocomplete color="primary" className='search-bar-text-field' fullWidth
+                id="provider"
+                name="provider"
+                options={optionsProvider}
+                onChange={(e, value) => {
+                  console.log(value);
+                  setFieldValue(
+                    "provider",
+                    value !== null ? value : initialValues.provider
+                  );
+                }}
+                renderInput={(params) => <TextField {...params} label="Sitio" name="provider" />}
+
+                value={values.provider}
+                //onBlur={handleBlur}
+                //error={touched.provider && Boolean(errors.provider)}   
+                isOptionEqualToValue={(option, value) => option.id === value.id}
+                getOptionLabel={option => option.name}
+              />
+
+              <TextField color="primary" fullWidth
+                label="Imagen"
+                id="images"
+                name="images"
+                className='search-bar-text-field'
+                value={values.images}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.images && Boolean(errors.images)}
+                helperText={touched.images && errors.images}
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">https://</InputAdornment>,
+                }}
+              />
+
+              <TextField color="primary" fullWidth
+                label="Link del articulo"
+                id="url"
+                name="url"
+                className='search-bar-text-field'
+                value={values.url}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.url && Boolean(errors.url)}
+                helperText={touched.url && errors.url}
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">https://</InputAdornment>,
+                }}
+              />
+
+              <Button variant="contained" color="success" startIcon={<SaveIcon />} type="submit" >GUARDAR</Button>
+            </div>
+          </div>
+        </Form>
+      )}
     </Formik>
-)};
+  );
+}
 
-export default Add;
+export default Add
