b08d878e3d3547e3d2d2390a41e9764d8a104d08
NinaSol <ninamarotta18@gmail.com>
2023-11-01 21:35:04 -0300

    agrego listado de productos de amigos

--- social-wishlist/src/components/ProductsCarrousel/ProductsCarrousel.jsx
+++ social-wishlist/src/components/ProductsCarrousel/ProductsCarrousel.jsx
@@ -6,27 +6,66 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { H2 } from "../Styles.css";
 import "./ProductsCarrousel.css";
 import { Box, CircularProgress } from "@mui/material";
-import { addProductToUser } from "../../firebase/queries/products";
+import {
+  addProductToUser,
+  getAllProducts,
+  getProductsOfUser,
+} from "../../firebase/queries/products";
 import { db } from "../../firebase";
 import { useNavigate, Link } from "react-router-dom";
 import { incrementPage, resetPage } from "../../actions/products";
 import { useEffect, useState } from "react";
+import { bool } from "prop-types";
 
-const ProductsCarrousel = () => {
+const ProductsCarrousel = ({ isFriendsTab }) => {
   const { products, loading, page } = useSelector((state) => state.products);
   const { user, loggedIn } = useSelector((state) => state.user);
   const [productsSlider, setProductsSlider] = useState();
+  const [friendsUids, setFriendsUids] = useState(user.friends);
+  const [friendsProducts, setFriendsProducts] = useState([]);
+  const finalProducts = isFriendsTab ? friendsProducts : products;
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+
   useEffect(() => {
-    if (!loading && products.length > 0) {
-      setProductsSlider([products[page], products[page + 1]]);
+    if (user.friends) {
+      setFriendsUids(Object.keys(user.friends));
     }
-  }, [page, loading, products]);
+  }, [user?.friends]);
+
+  useEffect(() => {
+    if (isFriendsTab) {
+      friendsUids?.forEach((uid) => {
+        getProductsOfUser(uid)
+          .then((products) => {
+            const mappedProducts = products.map((product) => ({
+              ...product,
+              uid,
+            }));
+            setFriendsProducts((friendsProducts) => [
+              ...friendsProducts,
+              ...mappedProducts,
+            ]);
+          })
+          .catch((error) => {
+            console.error("Error fetching friend products:", error);
+          });
+      });
+    } else {
+      dispatch(getAllProducts());
+    }
+  }, [dispatch, isFriendsTab, friendsUids]);
+
+  useEffect(() => {
+    if (!loading && finalProducts.length > 0) {
+      setProductsSlider([finalProducts[page], finalProducts[page + 1]]);
+    }
+  }, [page, loading, finalProducts]);
 
   const nextPage = () => {
-    if (page === products.length - 1) {
+    if (page === finalProducts.length - 1) {
       dispatch(resetPage());
     } else {
       dispatch(incrementPage());
@@ -43,7 +82,7 @@ const ProductsCarrousel = () => {
   const handleAddToWishlist = async () => {
     if (loggedIn) {
       try {
-        addProductToUser(db, products[page], user.uid);
+        addProductToUser(db, finalProducts[page], user.uid);
       } catch (error) {
         // show error dialog? and prompt retry?
       }
@@ -58,7 +97,6 @@ const ProductsCarrousel = () => {
         hiddenSlide[0].style.transform = "translate(-700px,-700px)";
         nextPage();
       }, 300);
-
     } else {
       // show dialog to user to login.
       navigate("/login");
@@ -67,53 +105,49 @@ const ProductsCarrousel = () => {
 
   const handleSkipProduct = () => {
     if (loggedIn) {
-      
       const hiddenSlide = document.getElementsByClassName("hidden-product");
       const currentSlide = document.getElementsByClassName("carousel-slider");
       currentSlide[0].style.transform = "translate(-500px,500px)";
       hiddenSlide[0].style.transform = "translate(-700px,-700px)";
-
 
       setTimeout(() => {
         currentSlide[0].style.transform = "translate(0)";
         hiddenSlide[0].style.transform = "translate(-700px,-700px)";
         nextPage();
       }, 300);
-
     } else {
       navigate("/login");
     }
   };
 
   const renderSlider = () => {
-    if(productsSlider && productsSlider.length > 0) {
-
-      return  productsSlider.map((product, i) => {
+    if (productsSlider && productsSlider.length > 0) {
+      return productsSlider.map((product, i) => {
         return (
-        <Link
-          className={`carousel-slider ${i === 1 && "hidden-product"}`}
-          to={`/product/${products[page].id}`}
-          key={i}
-        >
-          <Carousel showThumbs={false} showStatus={false}>
-            {product.images.map((img, i) => (
-              <img
-                key={i}
-                src={img}
-                style={{
-                  objectFit: "cover",
-                  height: "100%",
-                  objectPosition: "center center",
-                  borderRadius: "14px",
-                }}
+          <Link
+            className={`carousel-slider ${i === 1 && "hidden-product"}`}
+            to={`/product/${finalProducts[page].id}`}
+            key={i}
+          >
+            <Carousel showThumbs={false} showStatus={false}>
+              {product.images.map((img, i) => (
+                <img
+                  key={i}
+                  src={img}
+                  style={{
+                    objectFit: "cover",
+                    height: "100%",
+                    objectPosition: "center center",
+                    borderRadius: "14px",
+                  }}
                 />
-                ))}
-          </Carousel>
-          <H2 className="product-name">{compressString(product.name, 80)}</H2>
-        </Link>
-      );
-    });
-  }
+              ))}
+            </Carousel>
+            <H2 className="product-name">{compressString(product.name, 80)}</H2>
+          </Link>
+        );
+      });
+    }
   };
 
   if (loading) {
@@ -126,11 +160,9 @@ const ProductsCarrousel = () => {
 
   return (
     <>
-      {products.length > 0 ? (
+      {finalProducts.length > 0 ? (
         <div>
-          <div className="slider-container">
-          {renderSlider()}
-          </div>
+          <div className="slider-container">{renderSlider()}</div>
           <div
             className="button-group"
             style={{ justifyContent: "space-evenly" }}
@@ -144,6 +176,10 @@ const ProductsCarrousel = () => {
       )}
     </>
   );
+};
+
+ProductsCarrousel.propTypes = {
+  isFriendsTab: bool.isRequired,
 };
 
 export default ProductsCarrousel;
--- social-wishlist/src/views/Home/Home.jsx
+++ social-wishlist/src/views/Home/Home.jsx
@@ -1,44 +1,37 @@
 import { useEffect, useState } from "react";
-import SearchBar from "/src/components/SearchBar/SearchBar";
 import "./Home.css";
 import ProductsCarrousel from "../../components/ProductsCarrousel/ProductsCarrousel";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../../firebase/queries/products";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
 import { Container } from "../../components/Styles.css";
 
 const Home = () => {
-	const [tab, setTab] = useState("forYou");
-  const dispatch = useDispatch();
+  const [tab, setTab] = useState("forYou");
+  const isFriendsTab = tab === "friends";
 
-	useEffect(() => {
-		dispatch(getAllProducts());
-	}, [dispatch]);
-
-	return (
-		<Container>
-
-			<div className="content-box">
-				<div className="button-group">
-					<button
-						className={`btn ${tab === "friends" ? "btn-primary" : "btn-secondary"}`}
-						onClick={(e) => setTab("friends")}
-					>
-						Friends
-					</button>
-					<button
-						className={`btn ${tab === "forYou" ? "btn-primary" : "btn-secondary"}`}
-						onClick={(e) => setTab("forYou")}
-					>
-						For You
-					</button>
-				</div>
-
-				<ProductsCarrousel />
-			</div>
-		</Container>
-	);
+  return (
+    <Container>
+      <div className="content-box">
+        <div className="button-group">
+          <button
+            className={`btn ${
+				isFriendsTab ? "btn-primary" : "btn-secondary"
+            }`}
+            onClick={() => setTab("friends")}
+          >
+            Friends
+          </button>
+          <button
+            className={`btn ${
+				!isFriendsTab ? "btn-primary" : "btn-secondary"
+            }`}
+            onClick={() => setTab("forYou")}
+          >
+            For You
+          </button>
+        </div>
+        <ProductsCarrousel isFriendsTab={isFriendsTab} />
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
