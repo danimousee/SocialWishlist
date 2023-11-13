ca4eb564b9b1ea4389c8517fd36abff8c7ad9215
NinaSol <ninamarotta18@gmail.com>
2023-11-12 23:10:43 -0300

    agrego validaciones de imagenes url

--- social-wishlist/src/views/Add/Add.jsx
+++ social-wishlist/src/views/Add/Add.jsx
@@ -1,56 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Formik, Form } from "formik";
-import * as yup from 'yup';
+import * as yup from "yup";
 import { useDispatch } from "react-redux";
-import { addProduct } from '../../firebase/queries/products';
-import "./Add.css"
+import { addProduct } from "../../firebase/queries/products";
+import "./Add.css";
 
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import SaveIcon from '@mui/icons-material/SaveAlt';
-import Autocomplete from '@mui/material/Autocomplete';
-import InputAdornment from '@mui/material/InputAdornment';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SaveIcon from "@mui/icons-material/SaveAlt";
+import Autocomplete from "@mui/material/Autocomplete";
+import InputAdornment from "@mui/material/InputAdornment";
 
-import SuccessComponent from '../../components/SuccessComponent/SuccessComponent';
-import FailComponent from '../../components/FailComponent/FailComponent';
+import SuccessComponent from "../../components/SuccessComponent/SuccessComponent";
+import FailComponent from "../../components/FailComponent/FailComponent";
 import { Box, CircularProgress } from "@mui/material";
 
-
-const URL = /^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i
+const URL =
+  /^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
 
 const validationSchema = yup.object({
-    name: yup.string('Ingresa el nombre').required('El nombre es requerido'),
-    images: yup.string().matches(URL, 'Ingresa una URL valida').required('La imagen es requerida'),
-    url: yup.string().matches(URL, 'Ingresa una URL valida').required('El link es requerido'),
+  name: yup.string("Ingresa el nombre").required("El nombre es requerido"),
+  images: yup
+    .string()
+    .matches( /^(https?|ftp):\/\/(www\.)?.+\.(jpeg|jpg|png|gif|bmp|svg|webp)$/,
+    "Ingresa una URL valida")
+    .required("La imagen es requerida"),
+  url: yup
+    .string()
+    .matches(URL, "Ingresa una URL valida")
+    .required("El link es requerido"),
   //  provider: yup.required('Ingresa el provider')
 });
 
 const initialValues = {
-  name: '',
-  images: '',
-  url: '',
-  provider: { name: 'Mercado Libre', id: 'MELI' },
-  category: { name: 'Arte', id: 'Arte' }
+  name: "",
+  images: "",
+  url: "",
+  provider: { name: "Mercado Libre", id: "MELI" },
+  category: { name: "Arte", id: "Arte" },
 };
 
 const optionsProvider = [
-  { name: 'Mercado Libre', id: 'MELI' },
-  { name: 'Facebook', id: 'FACE' },
-  { name: 'Instagram', id: 'INSTA' },
-  { name: 'Otros', id: 'OTHER' },
+  { name: "Mercado Libre", id: "MELI" },
+  { name: "Facebook", id: "FACE" },
+  { name: "Instagram", id: "INSTA" },
+  { name: "Otros", id: "OTHER" },
 ];
 
 const optionsCategory = [
-  { name: 'Cars', id: 'Cars' },
-  { name: 'Technology', id: 'Technology' },
-  { name: 'Travel', id: 'Travel' },
-  { name: 'Toys', id: 'Toys' },
-  { name: 'Sports', id: 'Sports' },
-  { name: 'Games', id: 'Games' },
-  { name: 'Clothes', id: 'Clothes' },
-  { name: 'Food', id: 'Food' },
-  { name: 'Beauty', id: 'Beauty' },
-  { name: 'Home', id: 'Home' },
+  { name: "Cars", id: "Cars" },
+  { name: "Technology", id: "Technology" },
+  { name: "Travel", id: "Travel" },
+  { name: "Toys", id: "Toys" },
+  { name: "Sports", id: "Sports" },
+  { name: "Games", id: "Games" },
+  { name: "Clothes", id: "Clothes" },
+  { name: "Food", id: "Food" },
+  { name: "Beauty", id: "Beauty" },
+  { name: "Home", id: "Home" },
 ];
 
 const Add = () => {
@@ -59,9 +66,10 @@ const Add = () => {
   const [submitResult, setSubmitResult] = useState(null);
   const [productId, setProductId] = useState("");
 
-  const placeholderImage = "https://www.ivins.com/wp-content/uploads/2020/09/placeholder-1.png"
+  const placeholderImage =
+    "https://www.ivins.com/wp-content/uploads/2020/09/placeholder-1.png";
 
-  const submit = async values => {
+  const submit = async (values) => {
     setIsLoading(true);
     setSubmitResult(null);
 
@@ -80,10 +88,10 @@ const Add = () => {
 
       // TODO: Add to own wishlist here
 
-      setSubmitResult('success');
+      setSubmitResult("success");
     } catch (error) {
       // Manejar el error
-      setSubmitResult('fail');
+      setSubmitResult("fail");
     } finally {
       // Detener la carga después de un segundo, independientemente del resultado
       setTimeout(() => {
@@ -93,31 +101,46 @@ const Add = () => {
   };
 
   if (isLoading) {
-      <Box sx={{ display: "flex", justifyContent: "center", margin: "auto" }}>
-        <CircularProgress color="secondary" />
-      </Box>
+    <Box sx={{ display: "flex", justifyContent: "center", margin: "auto" }}>
+      <CircularProgress color="secondary" />
+    </Box>;
   }
 
-  if (submitResult === 'success') {
-    return <SuccessComponent productId={productId}/>; // Componente o mensaje de éxito
+  if (submitResult === "success") {
+    return <SuccessComponent productId={productId} />; // Componente o mensaje de éxito
   }
 
-  if (submitResult === 'fail') {
+  if (submitResult === "fail") {
     return <FailComponent />; // Componente o mensaje de fallo
   }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={submit} validationSchema={validationSchema}>
-      {({ handleChange, values, setFieldValue, handleBlur, handleReset, isSubmitting, errors, touched }) => (
-        <Form className='add-form'>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={submit}
+      validationSchema={validationSchema}
+    >
+      {({
+        handleChange,
+        values,
+        setFieldValue,
+        handleBlur,
+        handleReset,
+        isSubmitting,
+        errors,
+        touched,
+      }) => (
+        <Form className="add-form">
           <img src={values.images ? values.images : placeholderImage} />
           <div className="main-box">
             <div className="content-box">
-              <TextField color="primary" fullWidth
+              <TextField
+                color="primary"
+                fullWidth
                 label="Name"
                 id="name"
                 name="name"
-                className='search-bar-text-field'
+                className="search-bar-text-field"
                 value={values.name}
                 onChange={handleChange}
                 onBlur={handleBlur}
@@ -125,7 +148,10 @@ const Add = () => {
                 helperText={touched.name && errors.name}
               />
 
-              <Autocomplete color="primary" className='search-bar-text-field' fullWidth
+              <Autocomplete
+                color="primary"
+                className="search-bar-text-field"
+                fullWidth
                 id="category"
                 name="category"
                 options={optionsCategory}
@@ -135,15 +161,20 @@ const Add = () => {
                     value !== null ? value : initialValues.category
                   );
                 }}
-                renderInput={(params) => <TextField {...params} label="Category" name="category" />}
+                renderInput={(params) => (
+                  <TextField {...params} label="Category" name="category" />
+                )}
                 value={values.category}
                 //onBlur={handleBlur}
-                //error={touched.category && Boolean(errors.category)}   
+                //error={touched.category && Boolean(errors.category)}
                 isOptionEqualToValue={(option, value) => option.id === value.id}
-                getOptionLabel={option => option.name}
+                getOptionLabel={(option) => option.name}
               />
 
-              <Autocomplete color="primary" className='search-bar-text-field' fullWidth
+              <Autocomplete
+                color="primary"
+                className="search-bar-text-field"
+                fullWidth
                 id="provider"
                 name="provider"
                 options={optionsProvider}
@@ -153,19 +184,23 @@ const Add = () => {
                     value !== null ? value : initialValues.provider
                   );
                 }}
-                renderInput={(params) => <TextField {...params} label="Provider" name="provider" />}
+                renderInput={(params) => (
+                  <TextField {...params} label="Provider" name="provider" />
+                )}
                 value={values.provider}
                 //onBlur={handleBlur}
-                //error={touched.provider && Boolean(errors.provider)}   
+                //error={touched.provider && Boolean(errors.provider)}
                 isOptionEqualToValue={(option, value) => option.id === value.id}
-                getOptionLabel={option => option.name}
+                getOptionLabel={(option) => option.name}
               />
 
-              <TextField color="primary" fullWidth
+              <TextField
+                color="primary"
+                fullWidth
                 label="Image URL"
                 id="images"
                 name="images"
-                className='search-bar-text-field'
+                className="search-bar-text-field"
                 value={values.images}
                 onChange={handleChange}
                 onBlur={handleBlur}
@@ -176,11 +211,13 @@ const Add = () => {
                 // }}
               />
 
-              <TextField color="primary" fullWidth
+              <TextField
+                color="primary"
+                fullWidth
                 label="Store page URL"
                 id="url"
                 name="url"
-                className='search-bar-text-field'
+                className="search-bar-text-field"
                 value={values.url}
                 onChange={handleChange}
                 onBlur={handleBlur}
@@ -191,17 +228,19 @@ const Add = () => {
                 // }}
               />
 
-              <button className='btn btn-primary' type="submit" disabled={isLoading}>SAVE</button>
+              <button
+                className="btn btn-primary"
+                type="submit"
+                disabled={isLoading}
+              >
+                SAVE
+              </button>
             </div>
           </div>
         </Form>
       )}
     </Formik>
   );
-
 };
-  
- 
 
-
-export default Add
+export default Add;
